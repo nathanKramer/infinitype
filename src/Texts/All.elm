aeb570e383150ba27c325badef5e8ac6909f6485
sashiexists <myself@sashinexists.com>
2022-01-06 07:28:14 +1100

    Kanji corpus fixed

--- src/Texts/All.elm
+++ src/Texts/All.elm
@@ -3,7 +3,7 @@ module Texts.All exposing (..)
 import Dict
 import Texts.English10k as E10k
 import Texts.English1k as E1k
-import Texts.JapaneseHiraganaCommon as J0
+import Texts.JapaneseKanjiCommon as J0
 import Texts.JapaneseHiraganaCommon as J1
 import Texts.JapaneseKatakanaCommon as J2
 import Texts.MiscCode as Code
